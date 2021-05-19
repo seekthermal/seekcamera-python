cecb3bda62f3c354809e35c105f091ab17a3b39b
Michael S. Mead <mmead@thermal.com>
2021-05-19 11:46:28 -0700

    Add docstrings and more robust comments to seekcamera-opencv.py.
    This is an educational sample.
    
    Signed-off-by: Michael S. Mead <mmead@thermal.com>

--- examples/seekcamera-opencv.py
+++ examples/seekcamera-opencv.py
@@ -31,6 +31,8 @@ from seekcamera import (
 
 
 class Renderer:
+    """Contains camera and image data required to render images to the screen."""
+
     def __init__(self):
         self.busy = False
         self.frame = SeekFrame()
@@ -40,49 +42,101 @@ class Renderer:
 
 
 def on_frame(_camera, camera_frame, renderer):
-    # Notify the main thread that a new frame is available.
+    """Async callback fired whenever a new frame is available.
+
+    Parameters
+    ----------
+    _camera: SeekCamera
+        Reference to the camera for which the new frame is available.
+    camera_frame: SeekCameraFrame
+        Reference to the class encapsulating the new frame (potentially
+        in multiple formats).
+    renderer: Renderer
+        User defined data passed to the callback. This can be anything
+        but in this case it is a reference to the renderer object.
+    """
+
+    # Acquire the condition variable and notify the main thread
+    # that a new frame is ready to render. This is required since
+    # all rendering done by OpenCV needs to happen on the main thread.
     with renderer.frame_condition:
         renderer.frame = camera_frame.color_argb8888
         renderer.frame_condition.notify()
 
 
 def on_event(camera, event_type, event_status, renderer):
+    """Async callback fired whenever a camera event occurs.
+
+    Parameters
+    ----------
+    camera: SeekCamera
+        Reference to the camera on which an event occurred.
+    event_type: SeekCameraManagerEvent
+        Enumerated type indicating the type of event that occurred.
+    event_status: Optional[SeekCameraError]
+        Optional exception type. It will be a non-None derived instance of
+        SeekCameraError if the event_type is SeekCameraManagerEvent.ERROR.
+    renderer: Renderer
+        User defined data passed to the callback. This can be anything
+        but in this case it is a reference to the Renderer object.
+    """
     print("%s: %s" % (str(event_type), camera.chipid))
 
     if event_type == SeekCameraManagerEvent.CONNECT:
         if renderer.busy:
             return
 
+        # Claim the renderer.
+        # This is required in case of multiple cameras.
         renderer.busy = True
-        renderer.first_frame = True
-        camera.color_palette = SeekCameraColorPalette.TYRIAN
         renderer.camera = camera
 
+        # Indicate the first frame has not come in yet.
+        # This is required to properly resize the rendering window.
+        renderer.first_frame = True
+
+        # Set a custom color palette.
+        # Other options can set in a similar fashion.
+        camera.color_palette = SeekCameraColorPalette.TYRIAN
+
+        # Start imaging and provide a custom callback to be called
+        # every time a new frame is received.
         camera.register_frame_available_callback(on_frame, renderer)
         camera.capture_session_start(SeekCameraFrameFormat.COLOR_ARGB8888)
 
     elif event_type == SeekCameraManagerEvent.DISCONNECT:
+        # Check that the camera disconnecting is one actually associated with
+        # the renderer. This is required in case of multiple cameras.
         if renderer.camera == camera:
+            # Stop imaging and reset all the renderer state.
             camera.capture_session_stop()
-
             renderer.camera = None
             renderer.frame = None
             renderer.busy = False
 
     elif event_type == SeekCameraManagerEvent.ERROR:
-        print(str(event_status))
+        print(str(event_status) + ": " + camera.chipid)
+
+    elif event_type == SeekCameraManagerEvent.READY_TO_PAIR:
+        return
 
 
 def main():
     window_name = "Seek Thermal - Python OpenCV Sample"
     cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
 
+    # Create a context structure responsible for managing all connected USB cameras.
+    # Cameras with other IO types can be managed by using a bitwise or of the
+    # SeekCameraIOType enum cases.
     with SeekCameraManager(SeekCameraIOType.USB) as manager:
+        # Start listening for events.
         renderer = Renderer()
         manager.register_event_callback(on_event, renderer)
+
         while True:
-            # Wait a maximum of 150ms for each frame to come in.
-            # A condition variable is used to synchronize the access to the renderer.
+            # Wait a maximum of 150ms for each frame to be received.
+            # A condition variable is used to synchronize the access to the renderer;
+            # it will be notified by the user defined frame available callback thread.
             with renderer.frame_condition:
                 if renderer.frame_condition.wait(150.0 / 1000.0):
                     img = renderer.frame.data
@@ -93,11 +147,11 @@ def main():
                         cv2.resizeWindow(window_name, width * 2, height * 2)
                         renderer.first_frame = False
 
+                    # Render the image to the window.
                     cv2.imshow(window_name, img)
 
-            key = cv2.waitKey(1)
-
             # Process key events.
+            key = cv2.waitKey(1)
             if key == ord("q"):
                 break
 
