b2291704745849a75f2bf10b2467b29d9de34b6b
Michael S. Mead <mmead@thermal.com>
2021-05-25 10:14:37 -0700

    Initial pass at adding support for user defined color palettes
    Signed-off-by: Michael S. Mead <mmead@thermal.com>

--- seekcamera/camera.py
+++ seekcamera/camera.py
@@ -16,6 +16,7 @@
 
 import ctypes
 from enum import IntEnum
+from collections import namedtuple
 
 import numpy as np
 
@@ -166,12 +167,121 @@ class SeekCameraColorPalette(IntEnum):
     AMBER = 6
     HI = 7
     GREEN = 8
+    USER_0 = 9
+    USER_1 = 10
+    USER_2 = 11
+    USER_3 = 12
+    USER_4 = 13
 
     def __str__(self):
         return self.name
 
     def __repr__(self):
         return "SeekCameraColorPalette({})".format(self.value)
+
+
+class SeekCameraColorPaletteData(object):
+    """Collection of color values used to colorize a thermal image.
+
+    Each entry represents a component of a pixel. The values should be in ascending
+    order going from coldest to hottest temperature. It has 256 distinct entries.
+    Each entry is a tuple of color channels ordered as (b, g, r, a).
+
+    Examples
+    --------
+    Creating a new color palette data object with default data (all zeros).
+    >>> palette_data = SeekCameraColorPaletteData()
+
+    Creating a new color palette data object with specified data.
+    >>> palette_data = SeekCameraColorPaletteData([(255, 0, 0, 0)] * 256)
+
+    Iterating the values of a color palette data object.
+    >>> for value in palette_data: print(value)
+
+    Slicing a color palette object.
+    >>> palette_data[1:4] = [(255, 0, 0, 0), (0, 255, 0, 0), (0, 0, 255, 0)]
+    >>> print(palette_data[1:4])
+    """
+
+    def __init__(self, data=None):
+        """Creates a color palette data object.
+
+        Parameters
+        ----------
+        data: Optional[Iterable[Tuple[int, int, int, int]]]
+            Collection of tuples that specify the color values for the color palette.
+            It should have length 256; the tuples should be specified in (b, g, r, a)
+            order.
+        """
+        if data is None:
+            data = [(0, 0, 0, 0)] * 256
+
+        self._data = data
+        self._data_iter = 0
+
+    def __iter__(self):
+        """Iterates through the color values in the color palette data.
+
+        Returns
+        -------
+        SeekCameraColorPaletteData
+            Reference to the color palette data object to iterate.
+        """
+        self._data_iter = 0
+        return self
+
+    def __next__(self):
+        """Gets the next color value in the current iteration.
+
+        Returns
+        -------
+        Tuple[int, int, int, int]
+            The next color palette value in (b, g, r, a) order.
+
+        Raises
+        ------
+        StopIteration
+            If at the end of the collection.
+        """
+        if self._data_iter >= len(self._data):
+            raise StopIteration
+
+        result = self._data[self._data_iter]
+        self._data_iter += 1
+        return result
+
+    def __getitem__(self, key):
+        """Gets an color value or slice of color values.
+
+        Parameters
+        ----------
+        key: Union[slice, int]
+            Either a slice or a single index used to get the color values.
+        
+        Returns
+        -------
+        Union[List[Tuple[int, int, int, int]], Tuple[int, int, int, int]]
+            Either a slice of color values or a single color value.
+        """
+        if isinstance(key, slice):
+            return self._data[key.start : key.stop : key.step]
+        else:
+            return self._data[key]
+
+    def __setitem__(self, key, data):
+        """Sets an color value or slice of color values.
+
+        Parameters
+        ----------
+        key: Union[slice, int]
+            Either a slice or a single index used to set the color values.
+        data: Union[List[Tuple[int, int, int, int]], Tuple[int, int, int, int]]
+            Either a slice of color values or a single color value.
+        """
+        if isinstance(key, slice):
+            self._data[key.start : key.stop : key.step] = data
+        else:
+            self._data[key] = data
 
 
 class SeekCameraAGCMode(IntEnum):
